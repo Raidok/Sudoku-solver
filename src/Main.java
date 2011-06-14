9fc6174c63bba8019a930bb4cbca2e850596bf26
Raido Kalbre <RKalbre@gmail.com>
2011-06-14 16:23:41 +0300

    ignoring .class files

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,17 @@ public class Main {
       
       Cells cells = new Cells(9);
       
-      cells.setCell(0, 0, 3);
-      cells.setCell(0, 1, 4);
-      cells.setCell(1, 0, 5);
+      cells.setCell(0, 0, 9);
+      cells.setCell(0, 1, 6);
+      cells.setCell(0, 2, 3);
+      cells.setCell(1, 0, 1);
+      System.out.println("SISESTATUD:");
       cells.output();
       
-      Solver solver = new Solver(cells);
+      Solution solution = new Solution(cells);
+      System.out.println("LAHENDUS:");
+      solution.output();
+      
    }
    
    
