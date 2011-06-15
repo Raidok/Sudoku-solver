74cca78b212f0bf6c01ccd264ab96c9631162377
Raido Kalbre <RKalbre@gmail.com>
2011-06-15 11:37:48 +0300

    Updated it with subfield checking and now it works, 16ms on C2D E6300

--- src/Solution.java
+++ src/Solution.java
@@ -8,7 +8,13 @@ public class Solution {
    public Solution(Cells cells) {
       this.cells = cells;
       int size = cells.getSize();
-      System.out.println("Lahendus leitud: "+solve(size, 0));
+      long start = System.currentTimeMillis();
+      if (solve(size, 0)) {
+         System.out.println("Solution found in "+(System.currentTimeMillis()-start)+" ms!");
+         output();
+      } else {
+         System.out.println("No solution.");
+      }
    }
    
    public void output() {
@@ -16,40 +22,32 @@ public class Solution {
    }
    
    private boolean solve(int size, int count) {
-      if (count-1 == (size*size)) return true;
+      if (count+1 == (size*size)) return true;
       int row = count / size;
       int col = count % size;
       count++;
       
-      
       if (cells.getCell(row, col) == 0) { // is cell preset or not?
          for (int val = 1; val <= size; val++) { // loop through valid values
             if (isLegal(row,col,  val)) { // can this value be put in current cell
-//               System.out.println("r"+row+" v"+col+ " = "+val);
                cells.setCell(row, col, val); // sets cell's value
                if (solve(size, count)) return true;
             }
          } // end of loop through valid values
-         if (cells.getCell(row, col) == 0) {
-//            System.out.println("r"+row+" v"+col+ " IKKA NULL");
-         }
+         cells.setCell(row, col, 0); // didn't find suitable value, reset cell
       } else { // next cell if this one is not empty
          if (solve(size, count)) return true;
       }
-      cells.setCell(row, col, 0); // reset cell
       return false;
    }
    
    private boolean isLegal(int row, int col, int value) {
       int i = 0, j = 0;
-//      int left = col / 3;  // TODO section check
-//      int right = left + 1;
-//      int top = row / 3;
-//      int bottom = top + 1;
+      int left = col / 3;
+      int top = row / 3;
       for (int[] rowOfCells : cells.getCells()) {
          for (int cellValue : rowOfCells) {
-            if ((j == row || i == col) && cellValue == value) {
-//               System.out.println("i:col "+i+":"+col+" /// j:row "+j+":"+row);
+            if ((j == row || i == col || (left == i/3 && top == j/3)) && cellValue == value) {
                return false;
             }
             i++;
