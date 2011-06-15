d3b2213fd9397a12277cbc86b43b687bb4842817
Raido Kalbre <RKalbre@gmail.com>
2011-06-15 10:26:10 +0300

    Solution update, runs forever tho

--- src/Solution.java
+++ src/Solution.java
@@ -16,33 +16,36 @@ public class Solution {
    }
    
    private boolean solve(int size, int count) {
+      if (count-1 == (size*size)) return true;
       int row = count / size;
       int col = count % size;
+      count++;
       
-//      System.out.println("rida "+row+" veerg "+col);
       
       if (cells.getCell(row, col) == 0) { // is cell preset or not?
          for (int val = 1; val <= size; val++) { // loop through valid values
-            if (isLegal(col, row, val)) { // can this value be put in current cell
+            if (isLegal(row,col,  val)) { // can this value be put in current cell
+//               System.out.println("r"+row+" v"+col+ " = "+val);
                cells.setCell(row, col, val); // sets cell's value
-               
-               if (count < (size*size)) {
-                  return solve(size, count);
-               } else {
-                  return true;
-               }
+               if (solve(size, count)) return true;
             }
          } // end of loop through valid values
-         return false;
+         if (cells.getCell(row, col) == 0) {
+//            System.out.println("r"+row+" v"+col+ " IKKA NULL");
+         }
+      } else { // next cell if this one is not empty
+         if (solve(size, count)) return true;
       }
-      
-      count++;
-      
-      return true;
+      cells.setCell(row, col, 0); // reset cell
+      return false;
    }
    
-   private boolean isLegal(int col, int row, int value) {
+   private boolean isLegal(int row, int col, int value) {
       int i = 0, j = 0;
+//      int left = col / 3;  // TODO section check
+//      int right = left + 1;
+//      int top = row / 3;
+//      int bottom = top + 1;
       for (int[] rowOfCells : cells.getCells()) {
          for (int cellValue : rowOfCells) {
             if ((j == row || i == col) && cellValue == value) {
